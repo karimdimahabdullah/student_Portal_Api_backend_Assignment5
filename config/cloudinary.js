5cb253d8b452b724bf33b4e2106c1737e0776749
karimdimahabdullah <karimdimahabdullah0@gmail.com>
2026-09-26 14:35:06 +0000

    made changes to my assigment

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -5,8 +5,7 @@ import { v2 as cloudinary } from 'cloudinary';
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-  secure: true, 
+  api_secret: process.env.API_SECRET
 });
 
 export default cloudinary
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,12 +1,8 @@
-import multer from "multer"
 
 export function errorHandler(err, req, res, next) {
-  if (err instanceof multer.MulterError) {
-    if (err.code === 'LIMIT_FILE_SIZE') {
-      return res.status(413).json({ error: 'Max file size is 10 MB.' });
-    }
-    return res.status(400).json({ error: err.message }); // e.g. LIMIT_UNEXPECTED_FILE = wrong field name
-  }
-  console.error(err);
-  res.status(500).json({ error: 'Something went wrong.' }); // don't leak internals
+  console.error(err.stack);
+  const status = err.status || 500;
+  res.status(status).json({
+    error: err.message || 'Internal Server Error',
+  }); // don't leak internals
 }
